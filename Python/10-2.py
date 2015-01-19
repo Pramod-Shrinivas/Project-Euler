e252aeb7c14604804cf01feb7d4a233951581fe0
Pramod-Shrinivas <pramod.shrinivas@hotmail.com>
2015-01-19 18:52:01 +0530

    Create 10-2.py

---
+++ Python/10-2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+'''
+    Purpose: https://projecteuler.net/problem=10
+    Sieve of Erasthonese Algorithm used. Implemented using 7-3.py
+    @author:Pramod S   
+'''
+def main():
+    primes = [True] * 2000000
+    primes[0],primes[1] = [None] * 2
+    sum = 0
+    for ind,val in enumerate(primes):
+        if val is True and ind > 2000000 ** 0.5 + 1:
+            sum += ind
+        elif val is True:
+            primes[ind*2::ind] = [False] * ((1999999//ind) - 1)
+            sum += ind
+    print("result: {0}".format(sum))
+
+if __name__ == "__main__":
+    main()
